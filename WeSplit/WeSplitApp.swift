f1f4f6b5e43c767fc583e5842eddc8f0fcc17628
Ibrahim Abdul Aziz <ichbinhima@gmail.com>
2021-12-21 07:31:43 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Ibrahim Abdul Aziz on 21/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
